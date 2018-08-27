8618b2ca30fbb74b2b8ce156921576b230d64eb7
Hanzzy <hanzzy111@gmail.com>
2018-08-27 23:02:24 +0700

    new project

---
+++ src/main/java/com/hz/HzdemoApplication.java
@@ -0,0 +1,12 @@
+package com.hz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HzdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HzdemoApplication.class, args);
+	}
+}
